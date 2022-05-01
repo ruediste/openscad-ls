85d5d2194f6dfc337a617fcaed769cee603e26ad
Ruedi Steinmann <ruediste@gmail.com>
2022-05-01 20:55:59 +0200

    get grammar to work with some restriction in list continuations and let statements

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -1,10 +1,11 @@
 // import colors from 'colors';
 import { Command } from 'commander';
+import { isAstNode, streamAllContents } from 'langium';
 import { Input } from '../language-server/generated/ast';
 import { OpenScadLanguageMetaData } from '../language-server/generated/module';
 import { createOpenScadServices } from '../language-server/open-scad-module';
 import { extractAstNode } from './cli-util';
-// import { generateJavaScript } from './generator';
+// import { generateJavaScript } from './generator'; 
 
 export const generateAction = async (fileName: string, opts: GenerateOptions): Promise<void> => {
     // const services = createOpenScadServices().OpenScad;
@@ -16,6 +17,9 @@ export const generateAction = async (fileName: string, opts: GenerateOptions): P
 export const parseAction = async (fileName: string, opts: {}): Promise<void> => {
     const services = createOpenScadServices().OpenScad;
     const input = await extractAstNode<Input>(fileName, services);
+    for (const node of streamAllContents(input)) {
+        console.log(`Node ${node.$containerProperty} = ${node.$type} (${Object.entries(node).filter(([name, value]) => !name.startsWith('$') && !isAstNode(value) && !Array.isArray(value)).map(([name, value]) => name + "=" + value).join(",")})`)
+    }
     console.log("Input", input.statements.map(s => s.$type));
     // const generatedFilePath = generateJavaScript(model, fileName, opts.destination);
     // console.log(colors.green(`JavaScript code generated successfully: ${generatedFilePath}`));
